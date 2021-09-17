783814c9097cc01f3e8739c092e67c1d70482f8f
Edder Rojas <edder.rojas@gmail.com>
2021-09-16 23:06:07 -0600

    Desktop layout

--- components/ui/core/icons/index.js
+++ components/ui/core/icons/index.js
@@ -1,10 +1,15 @@
 import withSvg from '../../../hoc/withSvg';
+
+import FWBIcon from './fwb.svg';
+import FWBLongIcon from './fwb-long.svg';
 import ActivityIcon from './activity.svg';
 import ChevronLeftIcon from './chevron-left.svg';
 import ChevronRightIcon from './chevron-right.svg';
 import ChevronUpIcon from './chevron-up.svg';
 import ChevronDownIcon from './chevron-down.svg';
 
+export const FWB = withSvg(FWBIcon, 87, 32);
+export const FWBLong = withSvg(FWBLongIcon, 383, 30);
 export const Activity = withSvg(ActivityIcon, 22, 20);
 export const ChevronLeft = withSvg(ChevronLeftIcon, 24, 24);
 export const ChevronRight = withSvg(ChevronRightIcon, 24, 24);
--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,68 @@ import TotalCapitalCard from '../components/TotalCapitalCard'
 import TokenPriceCard from '../components/TokenPriceCard'
 import LPCard from '../components/LPCard'
 import DirectionalFadeIn from '../components/ui/core/DirectionalFadeIn'
+import { H2, H3 } from '../components/ui/core/Typography'
+import ConnectButton from '../components/ui/core/ConnectButton'
+import { FWB, FWBLong } from '../components/ui/core/icons'
 
 const Layout = styled.div`
   display: grid;
-  grid-template-columns: 1fr;
-  grid-row-gap: 20px;
-  max-width: 700px;
+  grid-template-columns: 1.2fr 2fr;
+  grid-gap: 20px;
+  padding: 0 48px;
   margin: 0 auto;
-`
+  height: 100vh;
 
-const HeaderLayout = styled.div`
+  > *:first-child {
+    border-right: 1px solid ${props => props.theme.colors.neutral700};
+    padding: 10px 48px 10px 0;
+  }
+`;
+
+const Hero = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-items: center;
-  padding: 80px 0 0;
-  > * {
-    padding: 10px 0;
+  justify-content: center;
+  height: 100%;
+
+  > *:first-child {
+    position: absolute;
+    top: 24px;
+    left: 0px;
+  }
+
+  > *:last-child {
+    display: grid;
+    grid-row-gap: 40px;
+    width: 100%;
   }
 `
 
-const TokenPriceLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: 20px;
+
+const TokenPriceLayout = styled(Hero)`
+  > *:first-child {
+    position: absolute;
+    top: 24px;
+    left: auto;
+    right: 0px;
+  }
+
+  > *:last-child {
+    padding-left: 38px;
+  }
 `
+
+const CardsWrapper = styled.div`
+  display: grid;
+  grid-gap: 10px;
+`;
+
+const PricesWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+`;
 
 const Home = () => {
   return (
@@ -41,21 +78,28 @@ const Home = () => {
       <main>
         <Layout>
           <DirectionalFadeIn direction="up">
-            <HeaderLayout>
-              <h1>Liquidity Mining</h1>
-              <h4>
-                Earn rewards for supplying liquidity for FWB Pro on Uniswap V3
-              </h4>
-            </HeaderLayout>
+            <Hero>
+              <FWB />
+              <div>
+                <FWBLong />
+                <h1>Liquidity Mining</h1>
+                <H3 color="neutral000">
+                  Earn rewards for supplying liquidity for FWB Pro on Uniswap V3
+                </H3>
+              </div>
+            </Hero>
           </DirectionalFadeIn>
           <DirectionalFadeIn direction="right" delay={200}>
             <TokenPriceLayout>
-              <TotalCapitalCard />
-              <TokenPriceCard />
+              <ConnectButton />
+              <CardsWrapper>
+                <PricesWrapper>
+                  <TokenPriceCard />
+                  <TotalCapitalCard />
+                </PricesWrapper>
+                <LPCard />
+              </CardsWrapper>
             </TokenPriceLayout>
-          </DirectionalFadeIn>
-          <DirectionalFadeIn direction="right" delay={400}>
-            <LPCard />
           </DirectionalFadeIn>
         </Layout>
       </main>
